8ad812642427b6aee4ca240bb7934ea716501ce8
George <george.pappas@live.com>
2020-11-09 11:45:42 -0600

    Update dice-roller.js

--- public/js/dice-roller.js
+++ public/js/dice-roller.js
@@ -9,40 +9,40 @@ function getRandomNumber() {
 function changeImage1() {
     var img = document.getElementById("image1");
     getRandomNumber()
-    img.src="/img/" + "Dice_" + ranNum + ".png";
+    img.src="/img/Dice_" + ranNum + ".png";
 }
 
 // Changes image for dice 2.
 function changeImage2() {
     var img = document.getElementById("image2");
     getRandomNumber()
-    img.src="/img/" + "Dice_" + ranNum + ".png";
+    img.src="/img/Dice_" + ranNum + ".png";
 }
 
 // Changes image for dice 3.
 function changeImage3() {
     var img = document.getElementById("image3");
     getRandomNumber()
-    img.src="/img/" + "Dice_" + ranNum + ".png";
+    img.src="/img/Dice_" + ranNum + ".png";
 }
 
 // Changes image for dice 4.
 function changeImage4() {
     var img = document.getElementById("image4");
     getRandomNumber()
-    img.src="/img/" + "Dice_" + ranNum + ".png";
+    img.src="/img/Dice_" + ranNum + ".png";
 }
 
 // Changes image for dice 5.
 function changeImage5() {
     var img = document.getElementById("image5");
     getRandomNumber()
-    img.src="/img/" + "Dice_" + ranNum + ".png";
+    img.src="/img/Dice_" + ranNum + ".png";
 }
 
 // Changes image for dice 6.
 function changeImage6() {
     var img = document.getElementById("image6");
     getRandomNumber()
-    img.src="/img/" + "Dice_" + ranNum + ".png";
+    img.src="/img/Dice_" + ranNum + ".png";
 }
